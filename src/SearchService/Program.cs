9fb884486a6ba4b2ae46267703aa590b0fcd6e2f
KhizarBajwa <thekhizarbajwa@gmail.com>
2025-07-01 03:30:48 -0400

    Inital setup completed for search services

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,53 +1,39 @@
-using AuctionService.Data;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
+
+// this is not need for swagger this is optional advance features
+builder.Services.AddOpenApi();
 
 // required for swagger
 builder.Services.AddEndpointsApiExplorer();
-
+// Add Swagger services
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
     {
-        Title = "Auction Service API",
+        Title = "Search Service API",
         Version = "v1",
-        Description = "API for managing auctions"
+        Description = "API for searching auctions"
     });
 });
-
-builder.Services.AddDbContext<AuctionDbContext>(opt =>
-{
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
 app.MapControllers();
-
-try
-{
-    DbInitializer.InitDb(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Error initializing database: {ex.Message}");
-}
 
 app.Run();
